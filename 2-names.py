d2cfc4ffeaac60449c38dd03b4f77108cb4869ff
Lorenzo Maturano <maturanolorenzo@gmail.com>
2019-12-04 17:46:58 -0300

    First Commit

---
+++ 2-names.py
@@ -0,0 +1,36 @@
+#Use Intention-Revealing Names
+
+d = 0 #Time Elapsed in days
+#If a name requires a comment, then the name does not reveal its intent
+elapsedTimeInDays = 0
+daysSinceCreation = 0
+daysSinceModfication = 0
+fileAgeInDays = 0
+#-----------------------------------------------------------------------
+
+#What is the purpouse of this code?
+
+def getThem(list):
+    list1 = []
+    for i in list:
+        if i[0] == 4:
+            list1.append(i)
+    return list1
+
+#Good names reveal purpouse
+
+def getFlaggedCells(gameBoard):
+    flaggedCells = []
+    for cell in gameBoard:
+        if cell['status'] == 'flagged':
+            flaggedCells.append(cell)
+    return flaggedCells
+
+#Creating a method for the if condition
+def getFlaggedCells(gameBoard):
+    flaggedCells = []
+    for cell in gameBoard:
+        if cell.isFlagged():
+            flaggedCells.append(cell)
+    return flaggedCells
+
